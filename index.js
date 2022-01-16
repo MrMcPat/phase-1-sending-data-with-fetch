1710669212e297de75b46b30154e85b30d41a079
Patrick Liang <pl204511194@yahoo.com>
2022-01-16 12:43:02 -0500

    commit tree 4b9caa3e8d93d94b4b104fe309149b758a647bc4

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your code here
+document.querySelector("#user-form").addEventListener("submit", handleUserData);
+
+function handleUserData(event) {
+    event.preventDefault();
+    let userName = event.target.enteredName.value;
+    let userEmail = event.target.enteredEmail.value;
+    submitData(userName, userEmail);
+}
+
+function submitData(name, email) {
+    // event.preventDefault();
+    // let userObj = {
+    //     name: event.target.enteredName.value,
+    //     email: event.target.enteredEmail.value
+    // }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email
+        }),
+    })
+    .then(res => res.json())
+    .then(user => renderUsers(user))
+    .catch(error => {
+        let p = document.createElement("p");
+        document.querySelector("#user-form").appendChild(p);
+        p.append(error.message);
+    });
+}
+
+function renderUsers(users) {
+    let div = document.createElement("div");
+    div.innerHTML = `<p>${users.id}</p>`
+    document.querySelector("#user-form").appendChild(div);
+}
